0625a9a449bf808668b346ab511c434389967565
omarhad <hadoussama@outlook.fr>
2022-07-29 22:25:02 +0200

    first commit / signup & login

---
+++ back/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
+const path = require('path');
+
+mongoose.connect('mongodb+srv://Omar-had:270495Oh.@cluster0.38uxqox.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
