a62c01dc1ee8128b39c7521ad0dca1025e75e960
omarhad <hadoussama@outlook.fr>
2022-08-04 22:43:31 +0200

    integration de helmet er express rate limit

--- back/app.js
+++ back/app.js
@@ -1,6 +1,8 @@
 const express = require('express'); // Express : framework web
 const dotenv = require("dotenv"); // Dotenv : gestionnaire de variables d'environnement
 const mongoose = require('mongoose'); // MongoDB : gestionnaire de base de données
+const helmet = require("helmet"); // Helmet : protection des headers HTTP
+const rateLimit = require('express-rate-limit'); // Express rate limit : limite de requêtes
 
 const path = require('path'); // Path : gestionnaire de chemins
 
@@ -11,7 +13,7 @@ const sauceRoutes = require('./routes/sauce'); // Import the sauce routes
 dotenv.config(); // Load the .env file
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_SRV,
+mongoose.connect(`${process.env.MONGODB_SRV}`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -19,6 +21,19 @@ mongoose.connect(process.env.MONGODB_SRV,
 
 // Parse JSON bodies
 app.use(express.json()); 
+
+// Use helmet to secure Express headers
+app.use(helmet());
+
+// Use rate limit to limit the number of requests from the same IP
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+// Apply to all requests
+app.use(limiter);
 
 // Allow CORS
 app.use((req, res, next) => {
--- back/middleware/auth.js
+++ back/middleware/auth.js
@@ -5,10 +5,14 @@ module.exports = (req, res, next) => {
        const token = req.headers.authorization.split(' ')[1]; // Get the token from the header
        const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET); // Decode the token
        const userId = decodedToken.userId; // Get the user id from the decoded token
-       req.auth = {
-           userId: userId
-       }; // Add the user id to the request object
-	next(); // Go to the next middleware
+       if (req.body.userId && req.body.userId !== userId) { // Check if the user id in the body is the same as the user id in the token
+           throw 'User id does not match token user id';
+       } else {
+            req.auth = {
+                userId: userId
+            }; // Add the user id to the request object
+            next(); // Call the next middleware
+        }
    } catch(error) {
        res.status(401).json({ error }); // If the token is not valid, send a 401 response
    }
